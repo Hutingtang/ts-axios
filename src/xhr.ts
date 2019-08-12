c621e511e17e84ca259b6bc75a79ed8338329ffd
cxl <shuanglongchen@yeah.net>
2019-08-12 17:43:48 +0800

    add axios error

--- src/xhr.ts
+++ src/xhr.ts
@@ -1,16 +1,24 @@
+
 import { AxiosRequestConfig, AxiosPromise, AxiosResponse } from './types';
 import { parseHeaders } from './helpers/header';
+import { createError } from './helpers/error';
 export default function xhr(config: AxiosRequestConfig): AxiosPromise {
     return new Promise((resolve, reject) => {
-        const { url, data = null, method = 'get', headers, responseType } = config;
+        const { url, data = null, method = 'get', headers, responseType, timeout } = config;
 
         const request = new XMLHttpRequest();
         if (responseType) {
             request.responseType = responseType;
         }
-        request.open(method.toUpperCase(), url, true);
+        if (timeout) {
+            request.timeout = timeout;
+        }
+        request.open(method.toUpperCase(), url!, true);
         request.onreadystatechange = function handleLoad() {
             if (request.readyState !== 4) {
+                return
+            }
+            if (request.status === 0) {
                 return
             }
             const responseHeaders = parseHeaders(request.getAllResponseHeaders());
@@ -23,9 +31,16 @@ export default function xhr(config: AxiosRequestConfig): AxiosPromise {
                 config,
                 request
             };
-            resolve(response);
+            handleResponse(response);
+            // resolve(response);
         }
-        Object.keys(headers).forEach((name) => {
+        request.onerror = function handleError() {
+            reject(createError('Network Error', config, null, request));
+        }
+        request.ontimeout = function handleTimeout() {
+            reject(createError(`Timeout of ${timeout} ms`, config, "Timeout Error", request));
+        }
+        Object.keys(headers).forEach(name => {
             if (data === null && name.toLowerCase() === 'content-type') {
                 delete headers[name];
             } else {
@@ -33,6 +48,13 @@ export default function xhr(config: AxiosRequestConfig): AxiosPromise {
             }
         })
         request.send(data);
+        function handleResponse(response: AxiosResponse): void {
+            if (response.status >= 200 && response.status < 300) {
+                resolve(response);
+            } else {
+                reject(createError(`Request failed with status code ${response.status}`, config, null, request, response))
+            }
+        }
     });
 
 }
